c292d21f8b7c15cf796bfd403efb7b381bf7cf92
Mohamed Asheik <33277633+asheikm@users.noreply.github.com>
2018-01-03 17:14:25 +0530

    Create configure.hpp

---
+++ configure.hpp
@@ -0,0 +1,36 @@
+#ifndef CONFIGURE_HPP
+#define CONFIGURE_HPP
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream> 
+
+using namespace std;
+
+
+class Configure
+{
+         private :
+               string HostIpaddr;
+               string HostName;
+
+               unsigned int Port;
+              
+               string tempConfigData[2];
+               string line;
+               static Configure *pconfig;
+
+               Configure();
+               void setIPaddress(string ip);
+               void setHostName(string hostname);
+               void setPort(unsigned int port);
+               
+
+        public :
+               static Configure* initConfig();
+
+};
+
+
+#endif
