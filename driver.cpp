07796a6b90f069433f502ac2930286a6bd7b8e2b
Mohamed Asheik <33277633+asheikm@users.noreply.github.com>
2018-01-05 04:42:56 +0530

    added calls to popluate data
    added calls to populate data

--- driver.cpp
+++ driver.cpp
@@ -3,5 +3,8 @@
 int main(int argc, char *argv[])
 {
    Configure *pconfigure = Configure::initConfig();
+   cout << "Ipaddress = " << pconfigure->getIPaddress() << endl;
+   cout << "HostName  = " << pconfigure->getHostName() << endl;
+   cout << "Port      = " << pconfigure->getPort() << endl;
    return 0
 }
